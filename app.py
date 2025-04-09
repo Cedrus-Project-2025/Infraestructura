9f60d688736790eabc27fd083a832a21faf943a4
damarisuwu1 <2109055@upy.edu.mx>
2025-04-08 18:22:54 -0600

    Implementacion de endpoint web/configs

--- app.py
+++ app.py
@@ -4,15 +4,13 @@ from flask import Flask, request
 from flask_restful import Api
 from flask_cors import CORS
 
-from Endpoints.General.usuarios import Usuarios
-from Endpoints.General.rclone import validar_rclone
-from Endpoints.Asistente_Virtual.chat import Chatbot_Response
-from Endpoints.Asistente_Virtual.configs import Chatbot_Config
+
+from Scripts.Asistente_Virtual.chat import Chatbot_Response
+from Scripts.Asistente_Virtual.configs import Obtener_Configs
 
 # ===== Validaciones iniciales
 load_dotenv()
 sys.stdout = sys.stderr
-validar_rclone()
 
 # ===== Configuracion API
 app = Flask(__name__)
@@ -21,9 +19,8 @@ api = Api(app)
 
 
 # ===== Endpoints
-api.add_resource(Usuarios,         "/api/g/usuarios")
-api.add_resource(Chatbot_Response, "/api/a/chat_response")
-api.add_resource(Chatbot_Config,   "/api/a/chat_config")
+api.add_resource(Chatbot_Response, "/api/a/chat")
+api.add_resource(Obtener_Configs,   "/api/a/configs")
 
 
 
