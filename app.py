2028983987ad779f652127fb4f73fc6f2130760f
damarisuwu1 <2109055@upy.edu.mx>
2025-04-26 21:06:26 -0600

    Cambio de nombre al archivo

--- app.py
+++ app.py
@@ -15,11 +15,11 @@ api = Api(app)
 
 
 # ===== Endpoints Asistente Virtual
-from Scripts.Asistente_Virtual.chat    import ChatbotResponse
-from Scripts.Asistente_Virtual.configs import ObtenerConfigs
+from Scripts.Asistente_Virtual.chat    import Chatbot_Response
+from Scripts.Asistente_Virtual.configs import Obtener_Configs
 
-api.add_resource(ChatbotResponse, "/api/a/chat")
-api.add_resource(ObtenerConfigs,  "/api/a/configs")
+api.add_resource(Chatbot_Response, "/api/a/chat")
+api.add_resource(Obtener_Configs,  "/api/a/configs")
 
 
 
@@ -35,7 +35,10 @@ api.add_resource(AudienciaAPI,     '/api/b/audiencia')
 
 
 # ===== Endpoints Web
-from Scripts.Desarrollo_Web.web_config import ContactoCentro_Config, OpcionesContacto_Config
-api.add_resource(ContactoCentro_Config,   '/api/w/contacto_centro')
-api.add_resource(OpcionesContacto_Config, '/api/w/opciones_contacto')
+from Scripts.Desarrollo_Web.general import ObtenerConfigsGeneral
 
+api.add_resource(ObtenerConfigsGeneral,     '/api/w/general')
+
+
+if __name__ == '__main__':
+    app.run(debug=True) 
